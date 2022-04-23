173ab7a92664d28ed4a6c919b7ecdb6029f5557c
unknown <fmyondaime2@gmail.com>
2022-04-23 00:53:52 -0300

    Practica integradora Formularios

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+})
+
 app.listen(PORT, () => console.log(`
 Servidor escuchando en el puerto ${PORT}
 http://localhost:${PORT}
